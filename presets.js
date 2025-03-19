f88ff2e806710be08d83e9217e0906c1d16cbcab
munkaud <munkaud@gmail.com>
2025-03-19 08:51:25 -0500

    additional presets to make use of additional actions

--- presets.js
+++ presets.js
@@ -1,11 +1,11 @@
 export function getPresetDefinitions(instance) {
 	return {
-		play_pause: {
+		b100_play_pause: {
 			type: 'button',
 			category: 'Playback',
 			name: 'Play/Pause',
 			style: {
-				text: 'Play/Pause',
+				text: 'Play\n/Pause\n⏯️',
 				size: '14',
 				color: '#FFFFFF',
 				bgcolor: '#000000',
@@ -14,7 +14,51 @@ export function getPresetDefinitions(instance) {
 				{
 					down: [
 						{
-							actionId: 'play_pause',
+							actionId: 'b100_play_pause',
+						},
+					],
+					up: [],
+				},
+			],
+			feedbacks: [],
+		},
+        b100_skip_forward: {
+			type: 'button',
+			category: 'Playback',
+			name: 'Skip Forward',
+			style: {
+				text: 'Next\n⏭️',
+				size: '14',
+				color: '#FFFFFF',
+				bgcolor: '#000000',
+			},
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'b100_skip_forward',
+						},
+					],
+					up: [],
+				},
+			],
+			feedbacks: [],
+		},
+        b100_skip_back: {
+			type: 'button',
+			category: 'Playback',
+			name: 'Skip Back',
+			style: {
+				text: 'Last\n⏮️',
+				size: '14',
+				color: '#FFFFFF',
+				bgcolor: '#000000',
+			},
+			steps: [
+				{
+					down: [
+						{
+							actionId: 'b100_skip_back',
 						},
 					],
 					up: [],
