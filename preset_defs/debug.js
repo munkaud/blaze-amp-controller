374fd5374606e98398308533d505c2eb3dd815fa
munkaud <munkaud@gmail.com>
2025-04-18 12:46:13 -0500

    Change Send Raw Command default to GET HARDWARE for safety

--- preset_defs/debug.js
+++ preset_defs/debug.js
@@ -1,43 +1,50 @@
 const { combineRgb } = require('@companion-module/base');
 
-module.exports = (self) => {
-  console.log('Debug presets accessed with instance config:', self.config);
-  return [
-    {
-      type: 'button',
-      category: 'Setup',
-      name: 'Initialize',
-      style: {
-        text: 'Init',
-        size: '14',
-        color: combineRgb(255, 255, 255),
-        bgcolor: combineRgb(0, 0, 0)
-      },
-      steps: [
-        {
-          down: [{ actionId: 'getConfig', options: {} }],
-          up: []
-        }
-      ],
-      feedbacks: []
+module.exports = (self) => [
+  {
+    type: 'button',
+    category: 'Debug',
+    name: 'Initialize',
+    style: {
+      text: 'Initialize',
+      size: '18',
+      color: combineRgb(255, 255, 255),
+      bgcolor: combineRgb(0, 0, 0),
     },
-    {
-      type: 'button',
-      category: 'Debug',
-      name: 'Send Raw Command',
-      style: {
-        text: 'Send Cmd',
-        size: '14',
-        color: combineRgb(255, 255, 255),
-        bgcolor: combineRgb(0, 0, 0)
+    steps: [
+      {
+        down: [
+          {
+            actionId: 'sendCommand',
+            options: { command: 'GET CONFIG', value: '' },
+          },
+        ],
+        up: [],
       },
-      steps: [
-        {
-          down: [{ actionId: 'sendCommand', options: { command: 'SET ZONE-A.DUCK.AUTO 1', value: '' } }],
-          up: []
-        }
-      ],
-      feedbacks: []
-    }
-  ];
-};
+    ],
+    feedbacks: [],
+  },
+  {
+    type: 'button',
+    category: 'Debug',
+    name: 'Send Raw Command',
+    style: {
+      text: 'Send Raw Cmd',
+      size: '14',
+      color: combineRgb(255, 255, 255),
+      bgcolor: combineRgb(0, 0, 0),
+    },
+    steps: [
+      {
+        down: [
+          {
+            actionId: 'sendCommand',
+            options: { command: 'GET HARDWARE', value: '' },
+          },
+        ],
+        up: [],
+      },
+    ],
+    feedbacks: [],
+  },
+];
